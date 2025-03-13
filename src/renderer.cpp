3c96039d4db1607c7fba80ed540047bec49cb80e
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-13 13:55:14 -0500

    Update renderer.cpp

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,31 +2,15 @@
 
 Renderer::Renderer(sf::RenderWindow* renderWindow)
     : renderWindow(renderWindow){
-    currentFPS = 0;
-    lastFPS = 0;
 
-    deltaTimeClock = sf::Clock();
-    deltaTime = sf::milliseconds(0);
-    currentDeltaTime = sf::milliseconds(0);
 }
 
 void Renderer::drawFrame(){
-        // Clear window, reset deltatime clock
-        deltaTimeClock.reset();
+        // Clear window
         Renderer::renderWindow->clear(sf::Color::Black);
 
         // Draw
 
-        // End render frame, capture delta time
+        // End render frame
         renderWindow->display();
-        deltaTime = deltaTimeClock.reset();
-        currentDeltaTime += deltaTime;
-        currentFPS++;
-
-        // FPS Calc
-        if(currentDeltaTime >= sf::seconds(1)){
-            currentDeltaTime = sf::Time::Zero;
-            lastFPS = currentFPS;
-            currentFPS = 0;
-        }
 }
